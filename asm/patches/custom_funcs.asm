08aa6c0d33196ea105623a7eb8267a8dfa2bb94b
Sizival <noahtjohnson12@gmail.com>
2026-01-18 16:49:46 -0500

    skip first 3 rounds of mail minigame

--- asm/patches/custom_funcs.asm
+++ asm/patches/custom_funcs.asm
@@ -143,6 +143,10 @@ li r4, 0x3B02 ; Saw cutscene before Puppet Ganon fight
 bl onEventBit__11dSv_event_cFUs
 li r4, 0x4002 ; Saw cutscene before Ganondorf fight
 bl onEventBit__11dSv_event_cFUs
+li r4, 0 ; 
+ori r4, r4, 0xC203 ; Register tracking mail sorting rounds with Koboli
+li r5, 3 ; Set to 3 to indicate Koboli's rounds are finished (triggers Baito to take over)
+bl setEventReg__11dSv_event_cFUsUc
 
 li r4, 0
 ori r4, r4, 0xBFFF ; Bitfield of which pigs you brought to Rose during the prologue (Pink, Speckled, Black)
